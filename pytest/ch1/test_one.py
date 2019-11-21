3e3fcf73ef09bf08ed04c564c8bf80e27c9dae00
paigechoi <redhater07>
2019-11-21 16:53:59 +0900

    first commit

---
+++ pytest/ch1/test_one.py
@@ -0,0 +1,12 @@
+def test_passing():
+    assert (1, 2, 3) == (1, 2, 3)
+
+"""
+from command line
+> pytest test_one.py
+
+. next to file name means that one test was run and it passed.
+if you need more information, use -v
+
+> pytest -v test_one.py
+"""
