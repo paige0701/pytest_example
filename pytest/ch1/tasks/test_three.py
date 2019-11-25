cd745d7a37931d046e11f4c4c54f56c5987469ec
paige0701 <paigechoi0701@gmail.com>
2019-11-25 09:16:04 +0900

    add test case

--- pytest/ch1/tasks/test_three.py
+++ pytest/ch1/tasks/test_three.py
@@ -9,3 +9,15 @@ def test_defaults():
     t2 = Task(None, None, False, None)
     assert t1 == t2
 
+
+def test_member_access():
+    t = Task('buy milk', 'brian')
+    assert t.summary == 'buy milk'
+    assert t.owner == 'brian'
+    assert (t.done, t.id) == (False, None)
+
+
+"""
+when you just want to run one test ~
+pytest test_three.py::test_member_access
+"""
